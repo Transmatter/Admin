877ebca80c3a407f1d8db45fc7573f6082d86139
oat431 <oat431@gmail.com>
2022-06-02 22:23:51 +0700

    [✨ setup] : the admin subsystem

---
+++ src/test/java/transmatter/platform/administration/AdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package transmatter.platform.administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
