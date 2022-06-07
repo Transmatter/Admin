1ebf269b453e32f733a1f7d007b319cad5c34465
oat431 <oat431@gmail.com>
2022-06-07 11:58:08 +0700

    [🔧 add] : optional url for frontned

--- src/main/java/transmatter/platform/administration/AdministrationApplication.java
+++ src/main/java/transmatter/platform/administration/AdministrationApplication.java
@@ -18,7 +18,7 @@ public class AdministrationApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","http://localhost:3300","http://localhost:5500")
                         .exposedHeaders("x-total-count");
             }
         };
