90421c190111e913663673028a3b2f2516b97c43
oat431 <oat431@gmail.com>
2022-06-07 12:58:15 +0700

    [✅ add] : test script

---
+++ src/test/java/transmatter/platform/administration/admin/AdminTest.java
@@ -0,0 +1,34 @@
+package transmatter.platform.administration.admin;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+public class AdminTest {
+    @Test
+    void adminLogin(){}
+
+    @Test
+    void adminLogout(){}
+
+    @Test
+    void adminRegister(){}
+
+    @Test
+    void adminUpdateTheirData(){}
+
+    @Test
+    void adminVerifyOtherAdmin(){}
+
+    @Test
+    void adminGetEmptyImageNotification(){}
+
+    @Test
+    void adminUpdateImageAlt(){}
+
+    @Test
+    void adminDeleteSpecificNew(){}
+}
