cdcf872c7c5d99e0a6c4584577399d83ad327033
oat431 <oat431@gmail.com>
2022-08-05 23:15:11 +0700

    [🧪 add] : content retrival testing

--- src/test/java/transmatter/platform/administration/AdministrationApplicationTests.java
+++ src/test/java/transmatter/platform/administration/AdministrationApplicationTests.java
@@ -1,13 +1,19 @@
 package transmatter.platform.administration;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import transmatter.platform.administration.contents.GetAllContentTest;
+import transmatter.platform.administration.contents.GetContentsByIdTest;
+import transmatter.platform.administration.contents.GetContentsBySourceTest;
+import transmatter.platform.administration.contents.SearchContentTest;
 
-@SpringBootTest
-class AdministrationApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+@Suite
+@SuiteDisplayName("Test Record for Administration Application")
+@SelectClasses({
+        GetContentsByIdTest.class,
+        GetAllContentTest.class,
+        GetContentsBySourceTest.class,
+        SearchContentTest.class
+})
+class AdministrationApplicationTests { }
